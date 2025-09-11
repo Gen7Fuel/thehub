28931e701f43e3ba7498912dcf42f5c5e92089e4
Mohammad <mohammad@gen7fuel.com>
2025-09-11 13:32:22 -0400

    hot reload

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,6 +5,8 @@ import tailwindcss from "@tailwindcss/vite";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import { resolve } from "node:path";
 
+const isDocker = process.env.VITE_DOCKER === "true";
+
 export default defineConfig({
   build: {
     chunkSizeWarningLimit: 1600,
@@ -30,9 +32,14 @@ export default defineConfig({
     watch: {
       usePolling: true,
     },
-    proxy: {
-      '/api': 'http://localhost:5000',
-      '/cdn': 'http://localhost:5001',
-    }
+    proxy: isDocker 
+    ? {
+        '/api': 'http://backend:5000',
+        '/cdn': 'http://cdn:5001',
+      }
+    : {
+        '/api': 'http://localhost:5000',
+        '/cdn': 'http://localhost:5001',
+      }
   }
 });
