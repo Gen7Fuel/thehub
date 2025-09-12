1f3fd4c22f01e90e537884731c3d4a4ef532a6e3
Daksh Jain <daksh@gen7fuel.com>
2025-09-12 17:16:40 -0400

    Permissions Progress EOD Friday

--- backend/models/Permission.js
+++ backend/models/Permission.js
@@ -4,7 +4,7 @@ const PermissionSchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true },
 });
 
-// ✅ This will use "permissions" automatically
+//This will use "permissions" automatically
 const Permission = mongoose.model("Permission", PermissionSchema);
 
 module.exports = Permission;
