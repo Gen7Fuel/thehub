f118f9f5e16552ed74b76d337ff06839613fb5fc
Daksh Jain <daksh@gen7fuel.com>
2026-01-14 13:28:50 -0500

    issue raised email cc updated

--- backend/routes/audit/auditTemplateRoutes.js
+++ backend/routes/audit/auditTemplateRoutes.js
@@ -989,8 +989,8 @@ router.post('/instance', async (req, res) => {
                       </div>
                     </div>
                   `;
-                  // const cc = ["daksh@gen7fuel.com", "ana@gen7fuel.com", "JDzyngel@gen7fuel.com", "kporter@gen7fuel.com", "michelle@gen7fuel.com"];
-                  const cc = ["daksh@gen7fuel.com"];
+                  const cc = ["daksh@gen7fuel.com", "ana@gen7fuel.com", "JDzyngel@gen7fuel.com", "kporter@gen7fuel.com", "michelle@gen7fuel.com"];
+                  // const cc = ["daksh@gen7fuel.com"];
                   await emailQueue.add("sendIssueEmail", { to, subject, text, html, cc });
                   console.log(`📨 Email queued for ${to}`);
                 } else {
