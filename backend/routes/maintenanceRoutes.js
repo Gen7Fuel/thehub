a12e6f5bfbfda6af9222d62a388f0918bd63eae5
Daksh Jain <daksh@gen7fuel.com>
2026-02-10 12:15:06 -0500

    MOD 2 Tue 10-02

--- backend/routes/maintenanceRoutes.js
+++ backend/routes/maintenanceRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const Maintenance = require('../models/Maintenance'); 
+const User = require('../models/User');
 
 // 1. POST: Create a new maintenance schedule
 router.post('/', async (req, res) => {
