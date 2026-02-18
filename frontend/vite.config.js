03a06f084e82300912c58fbb6f3fb8c77a9a9ec7
Daksh Jain <daksh@gen7fuel.com>
2026-02-18 13:19:07 -0500

    updated /auth to /login-auth

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -40,7 +40,7 @@ export default defineConfig({
     proxy: isDocker 
   ? {
       '/api': 'http://backend:5000',
-      '/auth': 'http://auth-backend:5005',
+      '/login-auth': 'http://auth-backend:5005',
       '/socket.io': {
         target: 'http://backend:5000',
         ws: true,  // Add WebSocket support
@@ -55,7 +55,7 @@ export default defineConfig({
     }
   : {
       '/api': 'http://localhost:5000',
-      '/auth': 'http://localhost:5005',
+      '/login-auth': 'http://localhost:5005',
       '/socket.io': {
         target: 'http://localhost:5000',
         ws: true,  // Add WebSocket support
