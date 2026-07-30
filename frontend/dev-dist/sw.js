3ed4a9c758856d27ee92df3f88c7ce0b2653c3d2
Daksh Gen7 <daksh@gen7fuel.com>
2026-07-30 09:36:32 -0400

    updated order file receiving handling on the order upload page

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.antnh41foig"
+    "revision": "0.sv2sdbsqe78"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
